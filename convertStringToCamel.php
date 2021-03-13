e62838c60f43c73dfd14e93f51436906f9aef04e
Mitirrli <phper.blue@gmail.com>
2021-03-13 09:06:58 +0800

    convertStringToCamel.php
    Convert string to camel case

---
+++ convertStringToCamel.php
@@ -0,0 +1,14 @@
+// Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+
+// Examples
+// "the-stealth-warrior" gets converted to "theStealthWarrior"
+// "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+<?php
+
+  function toCamelCase($str){
+    $arr = preg_split("/[-_]/", $str);
+    $first = array_shift($arr);
+  
+    return $first . implode('', array_map("ucfirst", $arr));
+  }
